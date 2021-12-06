40674dac1cf17ae0d6d682dd32bc9cdea67f591e
dawhoom <94390646+dawhoom@users.noreply.github.com>
2021-12-06 20:10:03 +0200

    Trimestru vidurkis
    trimestru vidurkio skaiciuokle

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -4,10 +4,31 @@ namespace GitTest
 {
     class Program
     {
+        public struct Pazymiai
+        {
+            public int PirmasTrimestras;
+            public int AntrasTrimestras;
+            public int TreciasTrimestras;
+
+            public Pazymiai(int pirmasTrimestras, int antrasTrimestras, int treciasTrimestras)
+            {
+                PirmasTrimestras = pirmasTrimestras;
+                AntrasTrimestras = antrasTrimestras;
+                TreciasTrimestras = treciasTrimestras;
+            }
+
+
+            public void pazymiuVidurkis()
+            {
+                int pazymiuVidurkis = (PirmasTrimestras + AntrasTrimestras + TreciasTrimestras) / 3;
+                Console.WriteLine($"Vidurkis: {pazymiuVidurkis}");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Goodbye!");
+            Pazymiai pazymys = new Pazymiai(5, 10, 7);
+            pazymys.pazymiuVidurkis();
         }
     }
 }
